365b9dfe69f95862208e30a9dd18d7081a1403b6
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2025-06-06 23:07:59 -0500

    Update Adapter.java
    Modificado para ser la version AOP del ejercicio A

--- Ejercicio_B/Adapter.java
+++ Ejercicio_B/Adapter.java
@@ -1,46 +1,54 @@
-package ec.edu.espol.Adapter
+package ec.edu.espol.Adapter;
 
-public class Gpay_acc{
-  /*
-    Codigo 
-  */
-  
-}
-public interface I_Cliente{
-  /*
-  Codigo
-  */
-
-  public void pagar(Gpay_acc acct);
-  
-}
-public class ImplementacionPagos implements I_Cliente{
-  /*
-    Codigo de ImplementacionPagos
-  */
-  public void pagar(Gpay_acc acct){
+public interface I_ReproductorAudio {
     /*
-    Codigo de pagar con Gpay_acc
+     Codigo de la interfaz
     */
-  }
+    public void reproducir(String archivo);
 }
-public class Pagos {
-    public void pagar(String Datos) {
-        /*
-    Codigo de pagar
+
+
+public class ImplementacionReproductor implements I_ReproductorAudio {
+    /*
+     Codigo de ImplementacionReproductor
     */
+    public void reproducir(String archivo) {
+        /*
+         Codigo de reproducir en ImplementacionReproductor
+        */
     }
 }
 
-public aspect Aspecto_adapter_pagos {
-  /*
-    Codigo del aspect
-    */
+
+public class ReproductorMP3 {
+    public void reproducirMP3(String nombreArchivo) {
+        /*
+         Codigo de reproducirMP3
+        */
+        System.out.println("[ReproductorMP3] Reproduciendo archivo MP3: " + nombreArchivo);
+    }
 }
 
-public class Cliente{
-/*
-    Codigo del cliente
+
+public aspect Aspecto_adapter_reproductor {
+    /*
+     Codigo del aspect
     */
-  
+
+    pointcut llamadaReproducir(String archivo):
+        execution(void I_ReproductorAudio.reproducir(String)) && args(archivo);
+
+    void around(String archivo) : llamadaReproducir(archivo) {
+        ReproductorMP3 adaptee = new ReproductorMP3();
+
+        adaptee.reproducirMP3(archivo);
+    }
+}
+
+
+public class Cliente {
+    /*
+     Codigo del cliente
+    */
+
 }
